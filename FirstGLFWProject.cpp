9f868cdefc7f139da6ef3e5f2951a93a9241fb3d
MGreenhill <fang88@rocketmail.com>
2024-05-13 19:20:25 -0500

    Vertex Colors
    -Removed greenValue change over time.
    -Added RGB color values to vertex points.
    -Added color attribute to vertex shader and assign color in fragment shader.
    -Created another Vertex Attribute to assign colors.

--- FirstGLFWProject.cpp
+++ FirstGLFWProject.cpp
@@ -6,20 +6,21 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 const char* vertexShaderSource = "#version 330 core\n"
-		"layout(location = 0) in vec3 aPos;\n"
-		"void main()\n"
+"layout(location = 0) in vec3 aPos;\n"
+"layout(location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
 		"{\n"
-		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"	
-		"}\0";
+	"	gl_Position = vec4(aPos, 1.0);\n"
+	"	ourColor=aColor;\n"
+	"}\0";
 
 const char* fragmentShaderSourceOrange = "#version 330 core\n"
 		"out vec4 FragColor;\n"
-		"uniform vec4 ourColor;\n"
+		"in vec3 ourColor;\n"
 		"void main()\n"
 		"{\n"
-		//"	ourColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
-		//"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-		"	FragColor = ourColor;\n"
+		"	FragColor = vec4(ourColor, 1.0);\n"
 		"}\n\0";
 
 int main() {
@@ -64,7 +65,6 @@ int main() {
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
-
 	//Fragment Shader
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSourceOrange, NULL);
@@ -94,14 +94,10 @@ int main() {
 
 	//setup for vertex data, buffers, and configure vertex attributes
 	float vertices[] = {
-		 -0.4f,  0.2f, 0.0f,
-		 -0.6f, -0.2f, 0.0f,
-		 -0.2f, -0.2f, 0.0f
-	};
-
-	unsigned int indices[] = {  // start with 0
-		0, 1, 3,
-		1, 2, 3
+		 //positions         //colors
+		 -0.4f,  0.2f, 0.0f,  1.0f, 0.0f, 0.0f,  //bottom right
+		 -0.6f, -0.2f, 0.0f,  0.0f, 1.0f, 0.0f,  //bottom left
+		 -0.2f, -0.2f, 0.0f,  0.0f, 0.0f, 1.0f   //top
 	};
 
 	//Create vertex buffer object and assign to GPU memory as static
@@ -116,9 +112,14 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO1);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
-	//Pass vertex data to the current vertex array
+	//Pass vertex Position to the current VAO
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	//Pass vertex color to current VAO
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
 
 	//render loop
 	//Keep window open untill told to close
@@ -132,12 +133,6 @@ int main() {
 
 		//Activate shader
 		glUseProgram(shaderProgram);
-
-		//update the uniform color
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
 		//Draw first triangle
 		glBindVertexArray(VAO1);
